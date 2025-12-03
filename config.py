b76b9e2010c4b6fe7a7424158c0d18af7972ac8b
noeEdwin <edwinnoe658@gmail.com>
2025-12-03 15:51:08 -0600

    Cambios para subir a netlify

--- config.py
+++ config.py
@@ -1,9 +1,10 @@
+import os
 
 DB_CONFIG = {
-    "dbname": "ModeloWEFE",
-    "user": "postgres",
-    "password": "9474609",
-    "host": "localhost",
-    "port": 5432,
-    "options": "-c search_path=ModeloWEFE,modelo_wefe",
+    "dbname": os.environ.get("DB_NAME", "ModeloWEFE"),
+    "user": os.environ.get("DB_USER", "postgres"),
+    "password": os.environ.get("DB_PASSWORD", "9474609"),
+    "host": os.environ.get("DB_HOST", "localhost"),
+    "port": int(os.environ.get("DB_PORT", 5432)),
+    "options": os.environ.get("DB_OPTIONS", "-c search_path=ModeloWEFE,modelo_wefe"),
 }
